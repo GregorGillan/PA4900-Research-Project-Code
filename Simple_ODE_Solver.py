1f28418497b316e7890f8fc8ea2bb215f6dcae71
GregorGillan <gbjgillan@gmail.com>
2021-01-19 15:41:43 +0000

    Test 3

---
+++ Simple_ODE_Solver.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from pylab import *
+
+#defining functions
+def modelRecovery(I, t):
+    r = 1/7
+    dIdt = - r * I  
+    return dIdt
+
+
+#inintial conditions
+I_0 = 0.1
+
+t = np.linspace(0, 20)
+
+#solve ODE
+I = odeint(modelRecovery, I_0, t)
+
+#plotting results
+
+plt.plot(t, I)
+
+show()
